baea47bd250d33fe2c94a2f42abc7738c19ba31e
gajohnso <gajohnso@192.168.1.127>
2015-03-02 09:47:15 -0500

    initial DB interface

---
+++ src/application/DBFunctions.java
@@ -0,0 +1,51 @@
+package application;
+
+import java.sql.*;
+
+public class DBFunctions {
+	Connection conn = null;
+	Statement stmt = null;
+	ResultSet rs = null;
+	
+	public int connect() {
+		try {
+			conn = DriverManager.getConnection("jdbc:mysql://carbon.dubrinksy.com/todoit", "greg", "horseboxfrog");
+			
+			System.out.println("connection successful");
+		}
+		catch (SQLException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+			return 1;
+		}
+		
+		return 0;
+	}
+	
+	public void disconnect() {
+		try {
+			conn.close();
+			
+			System.out.println("connection closed");
+		}
+		catch (SQLException e) {
+			System.out.println("SQLException: " + e.getMessage());
+			System.out.println("SQLState: " + e.getSQLState());
+			System.out.println("VendorError: " + e.getErrorCode());
+		}
+		
+	}
+	
+	public void upcomingTasks() {
+		
+	}
+	
+	
+	public static void Main (String[] args){
+		DBFunctions db1 = new DBFunctions();
+		
+		db1.connect();
+		db1.disconnect();
+	}
+
+}
