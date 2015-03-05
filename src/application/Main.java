91220a1b8be2095d6e43fbe2da3d6903cf1ced89
bwehrke <bwehrke@mtu.edu>
2015-03-05 10:10:40 -0500

    I added action events to all the buttons, made the main stage not resizeable along with set size.

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,8 +1,13 @@
 package application;
 
+import java.util.*;
+import javafx.*;
+
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.stage.Stage;
@@ -15,36 +20,61 @@ import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
+
+
+
 public class Main extends Application {
+
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			// This defines gui as a type of BorderPane
 			BorderPane gui = new BorderPane();
-			Scene scene = new Scene(gui, 600,600);
+			Scene scene = new Scene(gui, 600, 600);
 
-
-			// Use the right side of GUI for a flow pane
-			gui.setRight(addFlowPane());
-
-			// Use the center side of the GUI for a VBox
+			// Determine which parts of the GUI are used for what
 			gui.setCenter(addVBox());
-
 			gui.setBottom(addHBox());
+			gui.setRight(addFlowPane());
 
 			//Makes use of the css sheet
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 
-			//Makes window show and names the window
+			// Setting up the arguments and setting for your main stage
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("ToDoIt");
+			primaryStage.setWidth(600);
+			primaryStage.setHeight(600);
+			primaryStage.setResizable(false);
 			primaryStage.show();
+
+
 
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
 
+	public void startAddTask(Stage addTaskStage){
+		try {
+			BorderPane gui = new BorderPane();
+			Scene scene = new Scene(gui, 500, 400);
+
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+
+			addTaskStage.setScene(scene);
+			addTaskStage.setTitle("Add Task Manager");
+			addTaskStage.setWidth(500);
+			addTaskStage.setHeight(400);
+			addTaskStage.setResizable(false);
+			addTaskStage.show();
+
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 
 	public VBox addVBox() {
@@ -93,16 +123,54 @@ public class Main extends Application {
 		// This makes the flowpane work with the CSS
 		flow.getStyleClass().addAll("pane", "flow-tile");
 
-
+		// Make the four buttons to add to GUI
+		Button addTaskBut = new Button("Add Task");
+		Button removeTaskBut = new Button("Remove Task");
+		Button editTaskBut = new Button("Edit Task");
+		Button viewTaskBut = new Button("View Task");
 
 		// Add four buttons to the GUI
-		flow.getChildren().add(new Button("Add Task"));
-		
-		flow.getChildren().add(new Button("Remove Task"));
-		
-		flow.getChildren().add(new Button("Edit Task"));
-		
-		flow.getChildren().add(new Button("View Task"));
+		flow.getChildren().add(addTaskBut);
+		flow.getChildren().add(removeTaskBut);
+		flow.getChildren().add(editTaskBut);
+		flow.getChildren().add(viewTaskBut);
+
+		// Add the four button listerners 
+		addTaskBut.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				System.out.println("Add Task Event");
+
+				Stage addTaskStage = new Stage();
+
+				startAddTask(addTaskStage);
+
+
+
+
+			}
+		});
+
+		removeTaskBut.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				System.out.println("Remove Task Event");
+			}
+		});
+
+		editTaskBut.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				System.out.println("Edit Task Event");
+			}
+		});
+
+		viewTaskBut.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				System.out.println("View Task Event");
+			}
+		});
 
 
 		// Loads images maybe down the road
@@ -126,9 +194,11 @@ public class Main extends Application {
 
 		// Make HBOX use CSS
 		hbox.getStyleClass().addAll("pane", "hbox");
-		
+
 		// Rough Time stamp
-		hbox.getChildren().add(new Label("Sunday March 1st, 2015"));
+		Calendar calendar = new GregorianCalendar();		
+
+		hbox.getChildren().add(new Label(calendar.getTime().toString()));
 
 
 		return hbox;
