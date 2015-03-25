93f14c36f4dd23d43f8ecbc8e9d9dccf76287819
gajohnso <gajohnso@141.219.209.94>
2015-03-25 10:38:04 -0400

    Updated DBFunctions class to deal with restful web service and JSON parsing

--- src/application/DBFunctions.java
+++ src/application/DBFunctions.java
@@ -1,63 +1,62 @@
 package application;
 
-import java.sql.*;
 import java.util.*;
+import java.io.IOException;
+
+import org.json.*;
+import org.apache.http.*;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+
 
 public class DBFunctions {
-	
-	Connection conn = null;
-	PreparedStatement add = null;
-	PreparedStatement delete = null;
-	PreparedStatement modify = null;
-	PreparedStatement view = null;
-	ResultSet rs = null;
-
-	/**
-	 * logs into database
-	 * @return
-	 */
-	public int connect() {
-		try {
-			
-			String username = "greg";
-			String pass = "horseboxfrog";
-			
-			conn = DriverManager.getConnection(
-					"jdbc:mysql://Carbon.dubrinksy.com/todoit:80", username,
-					pass);
-			System.out.println("connection successful");
-		} catch (SQLException e) {
-			System.out.println(e.getMessage());
-			e.printStackTrace();
-			return 1;
-		}
-
-		return 0;
-	}
-
-	/**
-	 * closes connection with database
-	 */
-	public int disconnect() {
-		try {
-			conn.close();
-
-			System.out.println("connection closed");
-		} catch (SQLException e) {
-			System.out.println("SQLException: " + e.getMessage());
-			System.out.println("SQLState: " + e.getSQLState());
-			System.out.println("VendorError: " + e.getErrorCode());
-			
-			return 0;
-		}
-		
-		return 1;
-	}
 
 	/**
 	 * Takes in critical information about a task and creates a new entry in the
 	 * database
 	 * 
+	 * @param title
+	 * @param startTime
+	 * @param endTime
+	 * @param desc
+	 */
+	public static void addTask(String title, long startTime, long endTime,
+			String desc) {
+
+		try {
+			// Create http client
+			HttpClient client = HttpClientBuilder.create().build();
+
+			// Create url
+			String url = "carbon.dubrinsky.com/add_task.php?title=" + title
+					+ "&desc=" + desc + "&start=" + startTime + "&end="
+					+ endTime;
+
+			// Create Get Request
+			HttpGet get = new HttpGet(url);
+
+			// Execute request and capture response
+			client.execute(get);
+
+		} catch (ClientProtocolException e) {
+
+			// If exception, print stack trace
+			e.printStackTrace();
+
+		} catch (IOException e) {
+
+			// If exception, print stack trace
+			e.printStackTrace();
+		}
+
+	}
+
+	/**
+	 * Changes the properties of a Task
+	 * 
+	 * @param taskId
 	 * @param title
 	 * @param startDate
 	 * @param startTime
@@ -65,102 +64,33 @@ public class DBFunctions {
 	 * @param endTime
 	 * @param desc
 	 */
-	public int addTask(String title, java.sql.Date startDate, Time startTime,
-			java.sql.Date endDate, Time endTime, String desc) {
-		
-		int count;
-		
-		try {
-			// Initialize prepareStatment and insert variables
-			add = conn.prepareStatement("INSERT INTO TABLE task(task_title, "
-					+ "task_desc, start_date, start_time, end_date, end_time) "
-					+ "VALUES (?, ?, ?, ?, ?, ?);");
-			
-			add.setString(1, title);
-			add.setString(2, desc);
-			add.setDate(3, startDate);
-			add.setTime(4, startTime);
-			add.setDate(5, endDate);
-			add.setTime(6, endTime);
-			
-			// executes the update and determines the number of rows modified
-			count = add.executeUpdate();
-			
-			// if an incorrect number of rows are modified, rollback changes.  
-			// Otherwise, commit
-			if (count !=1){
-				System.out.println("Trying to modify " + count + " rows.");
-				conn.rollback();
-				return 2;
-			}
-			else{
-				conn.commit();
-				return count;
-			}
+	public void modifyTask(Integer taskId, String title, long startTime,
+			long endTime, String desc) {
 
-		} catch (SQLException e) {
-			System.out.println("SQLException: " + e.getMessage());
-			System.out.println("SQLState: " + e.getSQLState());
-			System.out.println("VendorError: " + e.getErrorCode());
-			
-			return 0;
-		}
-		
-	}
- /**
-  * Changes the properties of a Task
-  * 
-  * @param taskId
-  * @param title
-  * @param startDate
-  * @param startTime
-  * @param endDate
-  * @param endTime
-  * @param desc
-  */
-	public int modifyTask(Integer taskId, String title, java.sql.Date startDate,
-			Time startTime, java.sql.Date endDate, Time endTime, String desc) {
-		
-		int count;
-		
 		try {
-			
-			// Initializes prepareStatement and inserts variables
-			modify = conn.prepareStatement("UPDATE task SET task_title = ?,"
-					+ " task_desc = ?, start_date = ?, start_time = ?, "
-					+ "end_date = ?, end_time = ? WHERE task_id = ? ;");
-			
-			modify.setString(1, title);
-			modify.setString(2, desc);
-			modify.setDate(3, startDate);
-			modify.setTime(4, startTime);
-			modify.setDate(5, endDate);
-			modify.setTime(6, endTime);
-			modify.setInt(7, taskId);
-			
-			// executes the update and counts the rows affected
-			count = modify.executeUpdate();
-			
-			// if incorrect number of rows are affected, roll back.  
-			// Otherwise, commit
-			if(count != 1) {
-				System.out.println("trying to modify " + count + "lines");
-				conn.rollback();
-				
-				return 2;
-			}
-			else{
-				conn.commit();
-				
-				return count;
-			}
+			// Create http client
+			HttpClient client = HttpClientBuilder.create().build();
 
-		} catch (SQLException e) {
-			System.out.println("SQLException: " + e.getMessage());
-			System.out.println("SQLState: " + e.getSQLState());
-			System.out.println("VendorError: " + e.getErrorCode());
-			
-			return 0;
+			// Create url
+			String url = "carbon.dubrinsky.com/update_task.php?title=" + title
+					+ "&desc=" + desc + "&start=" + startTime + "&end="
+					+ endTime + "&taskID=" + taskId;
+
+			// Create Get Request
+			HttpGet get = new HttpGet(url);
+
+			// Execute request and capture response
+			client.execute(get);
+
+		} catch (ClientProtocolException e) {
+
+			// If exception, print stack trace
+			e.printStackTrace();
+
+		} catch (IOException e) {
+
+			// If exception, print stack trace
+			e.printStackTrace();
 		}
 
 	}
@@ -172,88 +102,91 @@ public class DBFunctions {
 	 * @param startDate
 	 * @param startTime
 	 */
-	public int deleteTask(Integer taskID) {
-		
-		int count;
-		
+	public void deleteTask(Integer taskID) {
 		try {
-			
-			// initializes prepareStatement and inserts variables
-			delete = conn.prepareStatement("DELETE FROM task WHERE task_id = ?;");
-			
-			delete.setInt(1, taskID);
-			
-			// Executes table update and counts affected rows
-			count = delete.executeUpdate();
-			
-			// if incorrect number of rows have been modified, roll back.
-			// Otherwise commit
-			if (count != 1) {
-				System.out.println("trying to modify " + count + "lines");
-				conn.rollback();
-				
-				return 2;
-			}
-			else{
-				conn.commit();
-				return count;
-			}
+			// Create http client
+			HttpClient client = HttpClientBuilder.create().build();
 
-		} catch (SQLException e) {
-			System.out.println("SQLException: " + e.getMessage());
-			System.out.println("SQLState: " + e.getSQLState());
-			System.out.println("VendorError: " + e.getErrorCode());
-			
-			return 0;
+			// Create url
+			String url = "carbon.dubrinsky.com/delete_task.php?taskID="
+					+ taskID;
+			// Create Get Request
+			HttpGet get = new HttpGet(url);
+
+			// Execute request and capture response
+			client.execute(get);
+
+		} catch (ClientProtocolException e) {
+
+			// If exception, print stack trace
+			e.printStackTrace();
+
+		} catch (IOException e) {
+
+			// If exception, print stack trace
+			e.printStackTrace();
 		}
 
 	}
+
 	/**
 	 * Returns up to the next 8 tasks in the table in order of start time
+	 * 
 	 * @return List<List<String>>
 	 */
-	public List<List<String>> viewUpcoming() {
-		
-		// Create date object for use in prepared statement
-		Calendar cal = Calendar.getInstance();
-		java.sql.Date date = new java.sql.Date(cal.getTime().getTime());
-		
+	public List<List<Object>> viewUpcoming() {
 		
 		try {
-			// Query DB to return next 8 tasks in order
-			view = conn.prepareStatement("SELECT * FROM task WHERE start_date >= ? desc LIMIT 8;");
-			
-			view.setDate(1, date);
-			
-			rs = view.executeQuery();
-			
-			// Retrieve column count from result set object
-			ResultSetMetaData meta = rs.getMetaData();
-			int numCols = meta.getColumnCount();
-			
-			// Initialize the list of tasks
-			List<List<String>> nextTasks = new ArrayList<>();
-			
-			// Populate the list
-			while (rs.next()) {
-				List<String> taskTuple = new ArrayList<>(numCols);
-				int i = 1;
-				
-				while (i <= numCols){
-					taskTuple.add(rs.getString(i++));
-				}
-				nextTasks.add(taskTuple);
-			}
-			
-			
-			return nextTasks;
+			// Create http client
+			HttpClient client = HttpClientBuilder.create().build();
 
-		} catch (SQLException e) {
-			System.out.println("SQLException: " + e.getMessage());
-			System.out.println("SQLState: " + e.getSQLState());
-			System.out.println("VendorError: " + e.getErrorCode());
+			// Create url
+			String url = "carbon.dubrinsky.com/read.php";
+					
+			// Create Get Request
+			HttpGet get = new HttpGet(url);
+
+			// Execute request and capture response
+			HttpResponse result = client.execute(get);
+			
+			ArrayList<ArrayList<Object>> tasks = new ArrayList<ArrayList<Object>>();
+			
+			JSONArray data = new JSONArray(result);
+			JSONObject element;
+			
+			for(int i = 0; i < data.length(); i++) {
+				element = data.getJSONObject(i);
+				String title = element.getString("task_title");
+				String desc = element.getString("task_desc");
+				long start = element.getLong("start_time");
+				long end = element.getLong("end_time");
+				int id = element.getInt("task_id");
+				
+				ArrayList<Object> ele = new ArrayList<Object>();
+				ele.add(title);
+				ele.add(desc);
+				ele.add(start);
+				ele.add(end);
+				ele.add(id);
+				
+				tasks.add(ele);
+			}
+
+		} catch (ClientProtocolException e) {
+
+			// If exception, print stack trace
+			e.printStackTrace();
+
+		} catch (IOException e) {
+
+			// If exception, print stack trace
+			e.printStackTrace();
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		
+
 		return null;
 
 	}
@@ -262,11 +195,11 @@ public class DBFunctions {
 	 * default constructor
 	 */
 	public DBFunctions() {
-	
+
 	}
-	
+
 	public static void Main(String[] args) {
-		
+
 	}
 
 }
