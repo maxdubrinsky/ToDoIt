7db00f7f7f04cfcccfb366149889a0655c9b0a2b
Nikita <njvelat@mtu.edu>
2015-04-10 00:28:23 -0400

    Did controller documentation and cleaned up formatting

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -9,6 +9,23 @@ public class Controller {
 
 	}
 
+	/**
+	 * This function takes in many strings from the GUI, converts them into a
+	 * format representing a task that is accepted by the database, then adds a
+	 * new task to the database.
+	 * 
+	 * Dates are converted into yyyymmddhhmmss longs.
+	 * 
+	 * @param title The title of the task.
+	 * @param desc A description of the task, max 255 charsm
+	 * @param priority The priority level of the taskm
+	 * @param eYear The year the task ends in.
+	 * @param eMonth The month the task ends in.
+	 * @param eDate The day the task ends in.
+	 * @param eHour The hour the task ends in.
+	 * @param eMin The minute the task ends in.
+	 * @param eAMPM Am or PM string, for a 12 hour time system.
+	 */
 	public static void addTask(String title, String desc, String priority,
 			String eYear, String eMonth, String eDate, String eHour,
 			String eMin, String eAMPM) {
@@ -82,17 +99,40 @@ public class Controller {
 
 		endT = Long.parseLong(concatEndT);
 
-		DBFunctions.addTask(title.replace("'", "''"), endT, desc.replace("'", "''"), Integer.parseInt(priority));
+		//Some sanitation
+		DBFunctions.addTask(title.replace("'", "''"), endT,
+				desc.replace("'", "''"), Integer.parseInt(priority));
 
 	}
-
 	
+
+	/**
+	 * This function takes in a Task object from the GUI and sends the id to the database for deletion.
+	 * 
+	 * @param toDelete The task object to delete.
+	 */
 	public static void deleteTask(Task toDelete) {
-		
 		db.deleteTask(toDelete.getID());
-		
 	}
 
+	/**
+	 * This function takes in a task object from the GUI as well as several strings
+	 * for which fields to modify, converts them into a format representing a task
+	 * that is accepted by the database, then modifies that existing task in the database.
+	 * 
+	 * Dates are converted into yyyymmddhhmmss longs.
+	 * 
+	 * @param toChange The Task object needing modifcation.
+	 * @param title The title of the task.
+	 * @param desc A description of the task, max 255 chars
+	 * @param priority The priority level of the task
+	 * @param eYear The year the task ends in.
+	 * @param eMonth The month the task ends in.
+	 * @param eDate The day the task ends in.
+	 * @param eHour The hour the task ends in.
+	 * @param eMin The minute the task ends in.
+	 * @param eAMPM Am or PM string, for a 12 hour time system.
+	 */
 	public static void modifyTask(Task toChange, String title, String desc,
 			String priority, String eYear, String eMonth, String eDate,
 			String eHour, String eMin, String eAMPM) {
@@ -165,12 +205,19 @@ public class Controller {
 		String concatEndT = eYear + eMonth + eDate + eHour + eMin + "00";
 
 		endT = Long.parseLong(concatEndT);
-		
 
-		 db.modifyTask(toChange.getID(), title.replace("'", "''"), endT, desc.replace("'", "''"), Integer.parseInt(priority));
+		//Some sanitation
+		db.modifyTask(toChange.getID(), title.replace("'", "''"), endT,
+				desc.replace("'", "''"), Integer.parseInt(priority));
 
 	}
 
+	
+	/**
+	 * This function simply fetches the list of upcoming tasks in the database and returns them to the GUI.
+	 * 
+	 * @return The list of upcoming tasks in the database
+	 */
 	public static ArrayList<Task> upcomingTasks() {
 		ArrayList<Task> taskList = new ArrayList<Task>();
 
@@ -179,14 +226,7 @@ public class Controller {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-
-		/*
-		 * List<String> sendTasks = new ArrayList<String>();
-		 * 
-		 * //temporary solution for (List<String> list: tasklis) {
-		 * sendTasks.add(list.get(0)); }
-		 */
-
+		
 		return taskList;
 	}
 }
